71faf89d4ab54f800af96a66e5621db2052f3de4
Phuripat Nilrueang <phuripatnilrueang@gmail.com>
2026-02-05 16:35:39 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://bearlab-backend-d9awbhgfe4c6abdq.southeastasia-01.azurewebsites.net/api',
+  baseURL: 'https://phuripat-backend-g5d5ezf4c2a3bah4.japaneast-01.azurewebsites.net/api',
   // baseURL: 'http://127.0.0.1:8000/api', // Local API
   headers: {
     'Content-Type': 'application/json',
